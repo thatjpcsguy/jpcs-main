25bc6146c6d9eb082dc3500492ba604b470c97e6
James Cooper-Stanbury <jpcs@freelancer.com>
2015-08-31 00:46:18 +1000

    all new python version with instagram api

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+import os
+import requests
+import pickle
+import datetime
+from os.path import isfile
+
+app = Flask(__name__)
+
+access_token = os.environ['INSTAGRAM_ACCESS_TOKEN']
+client_secret = os.environ['INSTAGRAM_CLIENT_SECRET']
+
+client_id = os.environ['INSTAGRAM_CLIENT_ID']
+
+user_id = 1901568934
+
+
+@app.route("/")
+def index():
+
+    filename = 'cached/' + str(datetime.date.today()) + '.instagram'
+    if isfile(filename):
+        with open(filename, 'rb') as f:
+            out = pickle.load(f)
+
+    else:
+        endpoint = 'https://api.instagram.com/v1/users/%s/media/recent/?access_token=%s' % (user_id, access_token)
+        print endpoint
+        r = requests.get(endpoint)
+        data = r.json()
+
+        out = []
+
+        for i in data['data']:
+            out.append({'url': i['images']['standard_resolution']['url'],
+                        'link': i['link'],
+                        'caption': i['caption']['text']})
+
+        with open(filename, 'wb') as f:
+            pickle.dump(out, f)
+
+    return render_template('index.html', data=out)
+
+
+if __name__ == "__main__":
+    app.run(port=4000, debug=True, host="0.0.0.0")
